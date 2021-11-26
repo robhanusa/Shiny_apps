9757b1018f632845f0646501a2a0c1c28140fcb8
Rob <rkhanusa@gmail.com>
2021-11-26 07:31:16 +0100

    improved graph and legend

--- House repayment calculator/server.R
+++ House repayment calculator/server.R
@@ -51,7 +51,11 @@ for (i in c(1:length(mortgage_expenditure_wo_fee))){
 #to compare to a baseline of cash
 cash <- rep(start_balance,term_mo+1)
 
-df <- data.frame(month,market_balance,cash,mortgage_expenditure,mortgage_expenditure_wo_fee)
+#vector for the x axis to help with the fill effects
+x_axis <- rep(0,term_mo+1)
+
+df <- data.frame(month,market_balance,cash,mortgage_expenditure,
+                 mortgage_expenditure_wo_fee, x_axis)
 
 #make df 'tidy' (ie arranged so all EUR values are in 1 column)
 #note that I give it a new name, since I'll need original df in geom_ribbon
@@ -60,16 +64,35 @@ df_tidy <- df %>% pivot_longer(!month,names_to = 'balance_type',values_to = 'eur
 #sort the df
 df_tidy <- df_tidy[order(df_tidy$balance_type,df_tidy$month),]
 
+#define colors
+darkerRed=rgba(255,0,0,.6)
+faintRed <- rgba(255,0,0,.3)
+faintOrange <- rgba(255,165,0,.3)
+faintBlue <- rgba(0,0,255,.2)
+
+
 ggplot(data = df_tidy, aes(x = month, y = euro, group = balance_type, 
                       linetype = balance_type))+
   geom_line()+
-  scale_linetype_manual(values=c('solid','dotted','longdash','blank'))+
-  geom_ribbon(data=df,aes(x=month,ymin=mortgage_expenditure_wo_fee, ymax=mortgage_expenditure),
-              fill='red',alpha=0.6,inherit.aes=FALSE)+
-  geom_ribbon(data=df,aes(x=month,ymin=cash, ymax=mortgage_expenditure_wo_fee),
-              fill='red',alpha=0.3,inherit.aes=FALSE)+
-  geom_ribbon(data=df,aes(x=month,ymin=mortgage_expenditure, ymax=market_balance),
-              fill='orange',alpha=0.3,inherit.aes=FALSE)+
+  #I'm choosing the linetypes and labels for legend below. the MANUAL function
+  #is used because I'm manually setting the linetype
+  scale_linetype_manual(values=c('solid','dotted','longdash','blank','blank'),
+                        labels=c('Cash Baseline','Market Return',
+                                 'Total Mortage Expenditure','',''))+
+  geom_ribbon(data=df,aes(x=month,ymin=mortgage_expenditure_wo_fee, ymax=mortgage_expenditure,fill='red',alpha=0.6),
+              inherit.aes=FALSE)+
+  geom_ribbon(data=df,aes(x=month,ymin=cash, ymax=mortgage_expenditure_wo_fee,fill='tomato',alpha=0.3),
+              inherit.aes=FALSE)+
+  geom_ribbon(data=df,aes(x=month,ymin=mortgage_expenditure, ymax=market_balance, fill='orange',alpha=0.3),
+              inherit.aes=FALSE)+
+  geom_ribbon(data=df,aes(x=month,ymin=x_axis, ymax=cash,fill='blue',alpha=0.2),
+              inherit.aes=FALSE)+
+  scale_fill_manual(name='',guide='legend',
+                    values=c('red'='red','tomato'='tomato', 'orange'='orange'),
+                    labels=c('Prepayment Penalty','Interest','Market Return Above Mortgage'))+
+  theme(legend.title=element_blank())+
   ylim(0,NA)
   
-#now, fill in area under cash line. I think this will be a different function (area fill?)
+#now, trying to get the legend for the fill colors to account for the alpha
+#I'm trying to save rgba colors so I can redefine them in the geom_ribbon()'s,
+#but i cant figure out how to save an rgba format in r
