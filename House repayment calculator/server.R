1c3de23f5396bb278361e732151d34cc0f06af37
Rob <rkhanusa@gmail.com>
2021-11-27 09:40:55 +0100

    improved graph, in final state

--- House repayment calculator/server.R
+++ House repayment calculator/server.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(ggplot2)
+library(plyr)
 
 #start balance in thousands
 start_balance <- 100
@@ -7,6 +8,7 @@ term_yr <- 10
 mortgage_rate_yr <- 0.015
 market_rate_yr <- 0.1
 early_pentalty <- 0.05
+include_market_return <- TRUE
 
 term_mo <- term_yr*12
 mortgage_rate_mo <- (mortgage_rate_yr+1)^(1/12)-1
@@ -59,40 +61,52 @@ df <- data.frame(month,market_balance,cash,mortgage_expenditure,
 
 #make df 'tidy' (ie arranged so all EUR values are in 1 column)
 #note that I give it a new name, since I'll need original df in geom_ribbon
-df_tidy <- df %>% pivot_longer(!month,names_to = 'balance_type',values_to = 'euro')
+df_tidy <- df %>% pivot_longer(!month,names_to = 'balance_type',
+                               values_to = 'euro')
 
 #sort the df
 df_tidy <- df_tidy[order(df_tidy$balance_type,df_tidy$month),]
 
 #define colors
-darkerRed=rgba(255,0,0,.6)
-faintRed <- rgba(255,0,0,.3)
-faintOrange <- rgba(255,165,0,.3)
-faintBlue <- rgba(0,0,255,.2)
-
+darkerRed <- rgb(255,0,0,alpha = .7*255,maxColorValue = 255)
+faintRed <- rgb(255,0,0,alpha = .3*255,maxColorValue = 255)
+faintOrange <- rgb(255,165,0,alpha = .3*255,maxColorValue = 255)
+faintBlue <- rgb(0,0,255,alpha = .2*255,maxColorValue = 255)
 
 ggplot(data = df_tidy, aes(x = month, y = euro, group = balance_type, 
-                      linetype = balance_type))+
-  geom_line()+
+                           linetype = balance_type))+
+  geom_line(size = 0.8)+
   #I'm choosing the linetypes and labels for legend below. the MANUAL function
   #is used because I'm manually setting the linetype
-  scale_linetype_manual(values=c('solid','dotted','longdash','blank','blank'),
-                        labels=c('Cash Baseline','Market Return',
+  scale_linetype_manual(values = c('solid','dotted','dashed','blank','blank'),
+                        labels = c('Cash Baseline','Market Return',
                                  'Total Mortage Expenditure','',''))+
-  geom_ribbon(data=df,aes(x=month,ymin=mortgage_expenditure_wo_fee, ymax=mortgage_expenditure,fill='red',alpha=0.6),
-              inherit.aes=FALSE)+
-  geom_ribbon(data=df,aes(x=month,ymin=cash, ymax=mortgage_expenditure_wo_fee,fill='tomato',alpha=0.3),
-              inherit.aes=FALSE)+
-  geom_ribbon(data=df,aes(x=month,ymin=mortgage_expenditure, ymax=market_balance, fill='orange',alpha=0.3),
-              inherit.aes=FALSE)+
-  geom_ribbon(data=df,aes(x=month,ymin=x_axis, ymax=cash,fill='blue',alpha=0.2),
-              inherit.aes=FALSE)+
-  scale_fill_manual(name='',guide='legend',
-                    values=c('red'='red','tomato'='tomato', 'orange'='orange'),
-                    labels=c('Prepayment Penalty','Interest','Market Return Above Mortgage'))+
-  theme(legend.title=element_blank())+
-  ylim(0,NA)
-  
-#now, trying to get the legend for the fill colors to account for the alpha
-#I'm trying to save rgba colors so I can redefine them in the geom_ribbon()'s,
-#but i cant figure out how to save an rgba format in r
+  geom_ribbon(data = df,aes(x = month,ymin = mortgage_expenditure_wo_fee, 
+                          ymax = mortgage_expenditure,fill = 'darkerRed'),
+              inherit.aes = FALSE)+
+  #note that I need to set show.legend = FALSE for all ribbons except 1, otherwise
+  #they overlap and the colors look darker
+  geom_ribbon(data = df,aes(x = month,ymin = cash, 
+                          ymax = mortgage_expenditure_wo_fee,fill = 'faintRed'),
+              inherit.aes = FALSE, show.legend = FALSE)+
+  geom_ribbon(data = df,aes(x = month,ymin = mortgage_expenditure, 
+                          ymax=market_balance, fill='faintOrange'),
+              inherit.aes = FALSE, show.legend = FALSE)+
+  geom_ribbon(data = df,aes(x = month,ymin = x_axis, 
+                          ymax = cash,fill = 'faintBlue'),
+              inherit.aes = FALSE,show.legend = FALSE)+
+  scale_fill_manual(name = '',guide = 'legend',
+                    values = c(darkerRed = darkerRed, faintRed = faintRed,
+                             faintOrange = faintOrange, faintBlue = faintBlue),
+                    labels = c('Prepayment Penalty','Interest',
+                               'Market Return Above Mortgage','Base'))+
+  #remove title, increase size, remove gray background on line types
+  theme(legend.title = element_blank(),legend.key.size = unit(1.5,"lines"), 
+        panel.border = element_blank(),
+        panel.grid = element_blank())+
+  scale_x_continuous(name = 'Month',limits = c(0,term_mo), expand = c(0,0))+
+  scale_y_continuous(name = 'Euro in thousands',
+                     limits = c(0,round_any(max(market_balance),50,f = ceiling)),
+                     expand = c(0,0))
+
+#now allow user to toggle include_market_return
