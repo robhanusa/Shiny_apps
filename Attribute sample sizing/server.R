085a969f1d24b32678b28c0a9f7b0960a4a84adf
Rob <rkhanusa@gmail.com>
2022-01-05 18:20:17 +0100

    Cleaned comments and updated some variable names

--- Attribute sample sizing/server.R
+++ Attribute sample sizing/server.R
@@ -2,6 +2,7 @@ library(shiny)
 library(tidyverse)
 library(plotly)
 
+#Sample size function----
 samples <- function(confidence, failures, rel) {
   round((0.5*qchisq(1-confidence,
                     2*(failures+1),
@@ -9,36 +10,41 @@ samples <- function(confidence, failures, rel) {
          /(1 - rel))
   )
 }
-#after I read more about plotly, I should to go the link below to get an example of 
-#a draggable moving horizontal line
-#https://community.rstudio.com/t/sliding-a-point-on-a-plot-rather-than-sliderinput-to-update-plot-in-shiny/16405/8
 
 server <- function(input,output,session) {
   output$p <- renderPlotly({
-    Reliability <- seq(0.9,.999,0.001)
-    Sample_size_needed_with_0_failures <- samples(input$cl,0,Reliability)
     
-    p <- ggplot(mapping=aes(x=Reliability*100))+
+    #create vectors that will become x and y axes
+    reliability_fraction <- seq(0.9,.999,0.001)
+    Sample_size_needed_with_0_failures <- samples(input$cl,0,reliability_fraction)
+    
+    Reliability <- reliability_fraction*100
+    
+    #Make plot----
+    p <- ggplot(mapping=aes(x=Reliability))+
       scale_y_log10() +
       xlab("% Reliability") +
       ylab("Samples needed")+
       theme(axis.title=element_text(size=14),
             axis.text=element_text(size=12))+
       geom_line(aes(y=Sample_size_needed_with_0_failures))
-    {
+      
+      #Add lines for different numbers of failures
       if (input$failures >= 1){
-        Sample_size_needed_with_1_failure <- samples(input$cl,1,Reliability)
+        Sample_size_needed_with_1_failure <- samples(input$cl,1,reliability_fraction)
         p <- p + geom_line(aes(y=Sample_size_needed_with_1_failure))
       } 
       if (input$failures >= 2){
-        Sample_size_needed_with_2_failures <- samples(input$cl,2,Reliability)
+        Sample_size_needed_with_2_failures <- samples(input$cl,2,reliability_fraction)
         p <- p + geom_line(aes(y=Sample_size_needed_with_2_failures))
       }
       if (input$failures >= 3){
-        Sample_size_needed_with_3_failures <- samples(input$cl,3,Reliability)
+        Sample_size_needed_with_3_failures <- samples(input$cl,3,reliability_fraction)
         p <- p + geom_line(aes(y=Sample_size_needed_with_3_failures))
       }
-      }
+      
     return(p)
   })
 }
+
+#to improve hovertext: https://gist.github.com/matt-dray/8e5d03e32318da90ee53cd35d81871c0
